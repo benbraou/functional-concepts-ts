f52d87d7ce069651f3256d7853343d968552cfe1
Oussama Ben Brahim <benbraou@gmail.com>
2016-12-04 21:07:49 +0100

    add all example

--- test/ramda/List.test.ts
+++ test/ramda/List.test.ts
@@ -3,6 +3,7 @@ import * as chai from "chai";
 const expect = chai.expect;
 
 describe('Ramda List functions', () => {
+
   describe('adjust', () => {
     it('should apply a function to an array at a given index', () => {
       let add = (a: number) => (b: number) => a + b;
@@ -13,4 +14,15 @@ describe('Ramda List functions', () => {
       expect(inputArray).to.deep.equal([1, 6, 4, 2]);
     });
   });
+
+  describe('all', () => {
+    it('should return true if all elements of the list match the predicate', () => {
+      let isPositive = (a: number) => a >= 0;
+      let positiveArray: number[] = [1, 6, 4, 2];
+      let notPositiveArray: number[] = [1, 6, -4, 2];
+      expect(R.all(isPositive, positiveArray)).is.true;
+      expect(R.all(isPositive, notPositiveArray)).is.false;
+    });
+  });
+
 });
